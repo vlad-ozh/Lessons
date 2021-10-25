8f397ed27339f6754a19c1c37299a74ad1c75940
vlad-ozh <vladozhekhovskyi@gmail.com>
2021-10-25 21:46:37 +0300

    task_2

---
+++ lesson_1/task_2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " _                 _        " << endl;
+    cout << "| |__   ___   ___ | | _____ " << endl;
+    cout << "| '_ \\ / _ \\ / _ \\| |/ / __|" << endl;
+    cout << "| |_) | (_) | (_) |   </\__ /\ "<< endl;
+    cout << "|_.__/ \\___/ \\___/|_|\\_\\___/" << endl;
+}
