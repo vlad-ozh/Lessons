ed0adfaba3d4c75af69c0af502913d5c8466994d
vlad-ozh <vladozhekhovskyi@gmail.com>
2021-10-25 21:48:29 +0300

    task_4

---
+++ lesson_1/Task_4/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, c, D, x, x1, x2;
+    cout << "enter a: ";
+    cin  >> a ;
+    cout << "enter b: ";
+    cin  >> b ;
+    cout << "enter c: ";
+    cin  >> c ;
+    cout << "a = " << a << ", " << "b = " << b << ", " << "c = " << c << endl;
+    D = pow(b, 2) - (4 * a * c);
+    cout << "diskriminant = " << D << endl;
+    if (D > 0){
+        x1 = (- b - sqrt(D) / 2 * a);
+        x2 = (- b + sqrt(D) / 2 * a);
+        cout << "x1 = " << x1 << ", " << "x2 = " << x2 << endl;
+    } else if (D < 0){
+        cout << "no solutions" << endl;
+    } else {
+        x = - b / 2 * a;
+        cout << "x = " << x << endl;
+    }
+
+}
