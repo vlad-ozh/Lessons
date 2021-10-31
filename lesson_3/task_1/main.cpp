d4d8e4bcbc21276c0b635ea25795b1853699b48f
vlad-ozh <vladozhekhovskyi@gmail.com>
2021-11-01 00:53:06 +0200

    task_1

--- lesson_3/task_1/main.cpp
+++ lesson_3/task_1/main.cpp
@@ -4,20 +4,16 @@ using namespace std;
 
 int main()
 {
-    int arr[10];
-    int num = 0;
-    short int a = 1;
+    int arr[10], num = 0;
     cout << "Enter the number you want to add to the elements of the array: ";
     cin >> num;
     for (short int i = 0; i < 10; i++){
-        cout << "Enter element array number " << a << ":";
+        cout << "Enter element array number " << i + 1 << ":";
         cin >> arr[i];
         arr[i] += num;
-        a++;
     }
-    a = 1;
+    cout << endl;
     for (short int i = 0; i < 10; i++){
-        cout << "Element array number " << a << ":" << arr[i]<< endl;
-        a++;
+        cout << "Element array number " << i + 1 << ":" << arr[i]<< endl;
     }
 }
