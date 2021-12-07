aad025fd3070c4c1dc295e723d8a8e201e33e5c4
vlad-ozh <vladozhekhovskyi@gmail.com>
2021-12-07 16:11:01 +0200

    optimization program

--- lesson_2/task_2/main.cpp
+++ lesson_2/task_2/main.cpp
@@ -5,14 +5,13 @@ using namespace std;
 int main()
 {
     unsigned long long  num;
-    unsigned short int  prog_1 = 0, prog_2 = 0, result = 0;
+    unsigned short int  progress = 0, result = 0;
     cout << "enter number: ";
     cin >> num;
     for (unsigned long long i = num; i > 0; i /= 10){
-        prog_1 = i % 10;
-        prog_2 = (i / 10) % 10;
-        if (prog_1 == prog_2) result += prog_1;
+        progress = i % 10;
+        if (progress == (i / 10) % 10) result += progress;
     }
-        if (prog_1 == num % 10) result += prog_1;
+        if (progress == num % 10) result += progress;
     cout << "your hash code: " << result;
 }
