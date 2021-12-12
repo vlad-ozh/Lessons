00578867689652ee9b7d9380e025676b1e630f19
vlad-ozh <vladozhekhovskyi@gmail.com>
2021-12-12 17:04:48 +0200

    optimization

--- lesson_4/task_1/main.cpp
+++ lesson_4/task_1/main.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main()
 {
-    char str[256] = {""};
+    char str[250] = "";
     int currentPunct = 0;
     cout << "Enter any string: ";
-    cin.getline(str, 256);
+    cin.getline(str, 250);
     for (int iter = 0; iter < strlen(str); iter++){
         if (str[iter] == ',' || str[iter] == '.' ||
             str[iter] == ';' || str[iter] == ':' ||
             str[iter] == '!' || str[iter] == '?' ||
             str[iter] == '-' || str[iter] == '"' ||
             str[iter] == '(' || str[iter] == ')')
-            currentPunct += 1;
+            currentPunct++;
     }
     if (strlen(str) == 0) cout << "String is empty..." << endl;
         else cout << "Number of punctuation marks: " << currentPunct << endl;
